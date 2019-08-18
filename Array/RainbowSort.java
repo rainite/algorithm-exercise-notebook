92207403b83c4970accee1ef564a8202bed1dbfb
rainite <kevinchenlife@gmail.com>
2019-08-18 18:01:21 -0400

    added question to rainbowsort

--- Array/RainbowSort.java
+++ Array/RainbowSort.java
@@ -1,3 +1,16 @@
+/**
+Given an array of balls, where the color of the balls can only be Red, Green or Blue, sort the balls such that all the Red balls are grouped on the left side, all the Green balls are grouped in the middle and all the Blue balls are grouped on the right side. (Red is denoted by -1, Green is denoted by 0, and Blue is denoted by 1).
+
+Examples
+
+{0} is sorted to {0}
+{1, 0} is sorted to {0, 1}
+{1, 0, 1, -1, 0} is sorted to {-1, 0, 0, 1, 1}
+Assumptions
+
+The input array is not null.
+**/
+
 public class RainbowSort {
   public int[] rainbowSort(int[] array) {
     // Write your solution here
